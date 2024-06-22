568dc5d79aa565846f6beaff52846def5a14038d
artisan24fullstack <160485169+artisan24fullstack@users.noreply.github.com>
2024-06-22 11:50:03 +0200

    blog postitem

--- cms/app/Models/Post.php
+++ cms/app/Models/Post.php
@@ -5,11 +5,18 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Support\Str;
 class Post extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'published_at' => 'datetime', //Call to a member function diffForHumans() on string
+
+    ];
+    public function author(){
+        return $this->belongsTo(User::class, "user_id");
+    }
     public function scopePublished($query){
         return $query->where('published_at','<=', Carbon::now());
     }
@@ -17,4 +24,13 @@ class Post extends Model
     public function scopeFeatured($query){
         return $query->where('featured', true);
     }
+    public function getExcerpt(){
+        return Str::limit(strip_tags($this->body,150));
+    }
+    public function getReadingTime(){
+        $mins = round(str_word_count($this->body) / 250);
+        return ($mins < 1 ) ? 1 : $mins;
+    }
+
+
 }
