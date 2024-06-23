214cd586cb9bd404e4f2803f8a017c3650af25d3
artisan24fullstack <160485169+artisan24fullstack@users.noreply.github.com>
2024-06-23 10:49:03 +0200

    panel admin category post

--- cms/app/Models/Post.php
+++ cms/app/Models/Post.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'image',
+        'body',
+        'published_at',
+        'featured',
+    ];
 
     protected $casts = [
         'published_at' => 'datetime', //Call to a member function diffForHumans() on string
@@ -17,6 +30,12 @@ class Post extends Model
     public function author(){
         return $this->belongsTo(User::class, "user_id");
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
     public function scopePublished($query){
         return $query->where('published_at','<=', Carbon::now());
     }
